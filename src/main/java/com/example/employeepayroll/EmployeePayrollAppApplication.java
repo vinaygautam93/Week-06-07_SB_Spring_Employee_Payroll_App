af9ceaab110f232b8764e8e3da4b0858ac619318
vinaygautam93 <vinaygautam935680@gmail.com>
2025-02-24 02:33:31 +0530

    [Vinay] Added EmployeePayrollAppApplication.java

---
+++ src/main/java/com/example/employeepayroll/EmployeePayrollAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.employeepayroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeePayrollAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeePayrollAppApplication.class, args);
+	}
+
+}
