00ce8226ff601474cef1eb42a3ae050ca242e856
vinaygautam93 <vinaygautam935680@gmail.com>
2025-02-24 16:29:12 +0530

    [Vinay] Added EmployeeService.java

--- src/main/java/com/example/employeepayroll/service/EmployeeService.java
+++ src/main/java/com/example/employeepayroll/service/EmployeeService.java
@@ -1,48 +1,28 @@
 package com.example.employeepayroll.service;
 
+import com.example.employeepayroll.dto.EmployeeDTO;
 import com.example.employeepayroll.model.Employee;
-import com.example.employeepayroll.repository.EmployeeRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class EmployeeService {
 
-    @Autowired
-    private EmployeeRepository employeeRepository;
+    private final List<Employee> employeeList = new ArrayList<>();
+
+    // Add an employee using DTO
+    public Employee addEmployee(EmployeeDTO employeeDTO) {
+        Employee employee = new Employee();
+        employee.setName(employeeDTO.getName());
+        employee.setSalary(employeeDTO.getSalary());
+        employeeList.add(employee);
+        return employee;
+    }
 
     // Get all employees
     public List<Employee> getAllEmployees() {
-        return employeeRepository.findAll();
-    }
-
-    // Add new employee
-    public Employee addEmployee(Employee employee) {
-        return employeeRepository.save(employee);
-    }
-
-    // Find employee by ID
-    public Employee getEmployeeById(Long id) {
-        return employeeRepository.findById(id).orElse(null);
-    }
-
-    // Update employee
-    public Employee updateEmployee(Long id, Employee employeeDetails) {
-        Employee employee = getEmployeeById(id);
-        if (employee != null) {
-            employee.setFirstName(employeeDetails.getFirstName());
-            employee.setLastName(employeeDetails.getLastName());
-            employee.setEmail(employeeDetails.getEmail());
-            employee.setSalary(employeeDetails.getSalary());
-            return employeeRepository.save(employee);
-        }
-        return null;
-    }
-
-    // Delete employee
-    public void deleteEmployee(Long id) {
-        employeeRepository.deleteById(id);
+        return employeeList;
     }
 }
